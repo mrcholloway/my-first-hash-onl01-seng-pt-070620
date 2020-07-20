ceda2fa61953a629be1ce945e7db934c6914b21b
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-20 02:46:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,4 +15,5 @@ end
 
  def adding
    shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" => 4, "pearl necklace" => 1}
+   
 end
